0b10226ab148febee104ed638940261d44b2a9ca
TKrosh <Krashenin2005@mail.ru>
2024-05-02 02:02:46 +0700

    bgtrhrtgehertyert

--- ConsoleApplication2/main.cpp
+++ ConsoleApplication2/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 const int N = 250;
 
@@ -41,7 +42,7 @@ int raise(int q) // возведение в степень, чтобы каждой позиции дать вес, использ
 }
 
 
-int heshing(char code[9])
+int heshing(char code[9], int col = 0)
 {
     int i;
     int sum = 0;
@@ -49,37 +50,72 @@ int heshing(char code[9])
     {
         sum += (code[i] - 0) * raise(i);
     }
-    sum = (sum) % N;
+    sum = (sum + col) % N;
     return sum;
 }
 
-int build_the_table(char* input_file_name)
+int rehash(int hash, table* t, info* elem)
 {
-    table Work_result;
-    for (int i = 0; i < N; i++)
+    int new_hash; //переменная для нового хеша
+    new_hash = (hash + 1) % N;
+    while (t->el[new_hash] && hash != new_hash && strcmp(t->el[new_hash]->code, elem->code))
     {
-        Work_result.el[i] = NULL;
+        new_hash = (new_hash + 1) % N;
     }
+    return new_hash;
+}
 
+int build_the_table(char* input_file_name, table* Work_result)
+{
     FILE* input_file;
     info elem;
     int hash;
 
+    for (int i = 0; i < N; i++)
+    {
+        Work_result->el[i] = NULL;
+    }
 
-    fopen_s(&input_file, "test.txt", "r");
+
+    fopen_s(&input_file, input_file_name, "r");
     if (!input_file)
         return 1;
+
+
     while (fscanf_s(input_file, "%s %s %d", elem.code, _countof(elem.code), elem.name, _countof(elem.name), &elem.amount) != EOF)
     {
         hash = heshing(elem.code); //значится, захешили
         printf("%d ", hash);
         //проверочка на наличие в таблице?
-        if (!Work_result.el[hash]) Work_result.el[hash] = &elem;
+        if (!Work_result->el[hash])
+        {//если с списках не значиться, то добавить
+            Work_result->el[hash] = new info;
+            *Work_result->el[hash] = elem;
+        }
+        else//теперь проверяем произощло ли совпадение хешей или это тот же товар
+        {
 
-        check(&Work_result);
+            if (!strcmp(Work_result->el[hash]->code, elem.code))//если этот товар уже есть то увеличиваем кол-во
+            {
+                Work_result->el[hash]->amount += elem.amount;
+            }
+            else
+            {
+                hash = rehash(hash, Work_result, &elem);
+                if (!Work_result->el[hash])//если этот товар уже есть то увеличиваем кол-во
+                {
+                    Work_result->el[hash] = new info;
+                    *Work_result->el[hash] = elem;
+                }
+                else
+                {
+                    Work_result->el[hash]->amount += elem.amount;
+                }
+            }
+        }
+
+        check(Work_result);
     }
-
-    //printf("%s %s %d\n", Work_result.el[113]->code, Work_result.el[113]->name, Work_result.el[113]->amount);
 
     fclose(input_file);
 };
@@ -87,8 +123,9 @@ int build_the_table(char* input_file_name)
 int main() {
     int c;
     char input_file_name[] = "test.txt";
+    table Work_result;
 
-    c = build_the_table(input_file_name);
+    c = build_the_table(input_file_name, &Work_result);
     if (c) {
         printf("0");
         return 0;
