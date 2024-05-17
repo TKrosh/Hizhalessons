418bb175a92dc342093a36b44921dd87812bb52d
nikul22 <steam.egor70@gmail.com>
2024-05-17 23:31:41 +0700

    started pz_5

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
-#include "stdio.h"
-#include "string.h"
+#include <stdio.h>
+#include <string.h>
+
 
 const int N = 16;
 
+
 struct info
 {
+    int hash_code;
     char code[9];
     char name[50];
     int amount;
+    int price;
 };
 
 
@@ -16,135 +20,48 @@ struct table
     info el[N];
 };
 
-//void check(table* tab)
-//{
-//    info elem;
-//    printf("\n");
-//    for (int i = 0; i < N; i++)
-//    {
-//        elem = tab->el[i];
-//        if (elem != NULL) printf("%d) %s %s %d\n", i, elem->code, elem->name, elem->amount);
-//
-//    }
-//}
 
-int pow(int q)
-{
-    int num = 1;
-    for (int i = 1; i <= q; i++)
-    {
-        num *= 2;
-    }
+int appending_table(table* work_result, char* test_name, char* prices) {
+    FILE* input_file = fopen(test_name, "r");
+    FILE* price_file;
+    info element_of_table, good_price;
+    int i = 0;
 
-    return num;
-}
+    if(!input_file)
+        return 1;
 
+    while(fscanf(input_file, "%d %s %s %d", &element_of_table.hash_code, element_of_table.code, element_of_table.name, &element_of_table.amount) != EOF){
+        price_file = fopen(prices, "r");
 
-int heshing(char code[9], int col = 0)
-{
-    int i;
-    int sum = 0;
-    for (i = 0; i < 8; i++)
-    {
-        sum += (code[i] - 0) * pow(i);
-    }
-    sum = (sum + col) % N;
-    return sum;
-}
+        if(!price_file)
+            return 1;
 
-int rehash(int hash, table* t, info* elem)
-{
-    int new_hash; //that is a new hash
-    new_hash = (hash + 1) % N;
-    while (strcmp(t->el[new_hash].code, "") && hash != new_hash && strcmp(t->el[new_hash].code, elem->code))
-    {
-        new_hash = (new_hash + 1) % N;
-    }
-    return new_hash;
-}
+        strcpy(good_price.code, "");
 
-void output(char* output_file_name, table* result) {
-    FILE* output_file = fopen(output_file_name, "w");;
-    info elem;
-    for (int i = 0; i < N; i++)
-    {
-        elem = result->el[i];
-        if (strcmp(elem.code, ""))
-
-            fprintf(output_file, "%d %d %s %s %d\n", i, heshing(elem.code), elem.code, elem.name, elem.amount);
-
-
-    }
-    fclose(output_file);
-}
-
-int build_the_table(char* input_file_name, table* Work_result)
-{
-    FILE* input_file = fopen(input_file_name, "r");;
-    info elem;
-    int hash, new_hash, res = 1;
-
-    if (!input_file)
-        return 0;
-
-    for(int i = 0; i < N; i++)
-    {
-
-        strcpy(Work_result->el[i].code, ""); // !!!!!!!!!!!!!!!!
-
-    }
-
-
-    while (fscanf(input_file, "%s %s %d", elem.code, elem.name, &elem.amount) != EOF && res)
-    {
-        hash = heshing(elem.code);
-        if (!strcmp(Work_result->el[hash].code, ""))
-
-            Work_result ->el[hash] = elem;
-        else
-        {
-
-            if (!strcmp(Work_result->el[hash].code, elem.code))
-                Work_result->el[hash].amount += elem.amount;
-            else
-            {
-                
-                new_hash = rehash(hash, Work_result, &elem);
-                if (hash == new_hash)
-                    res = 0;
-
-                if (!strcmp(Work_result->el[new_hash].code, ""))
-                    Work_result ->el[new_hash] = elem;
-
-
-                else
-                    Work_result->el[new_hash].amount += elem.amount;
-
+        while(fscanf(price_file, "%s %d", good_price.code, &good_price.price) != EOF && strcmp(element_of_table.code, good_price.code) <= 0){
+            if (!strcmp(element_of_table.code, good_price.code)){
+                work_result->el[i] = element_of_table;
+                work_result->el[i++].price = good_price.price;
             }
         }
-
-        //check(Work_result);
     }
 
     fclose(input_file);
-    return res;
+    fclose(price_file);
+    return 0;
+}
+
+void sorting_table();
 
 
-};
-
-int main() {
-
-    int c;
-    table Work_result;
-
-    c = build_the_table("test4.txt", &Work_result);
-    if (!c)
-    {
-        printf("0");
-        return 0;
+int main(){
+    table result;
+    int task_completed;
+    char test_name[] = "test1.txt";
+    char prices[] = "price_list.txt";
+    task_completed = appending_table(&result, test_name, prices);
+    if (task_completed){
+        return 1;
     }
 
-    output("result.txt", &Work_result);
-
-    return 0;
 }
